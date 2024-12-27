e343b2e97c0198bb27319fabbd1b7926b33ee0ea
Jay Vegad <143251488+Vegadjay@users.noreply.github.com>
2024-12-27 08:54:54 +0530

    Change Some Code

--- src/pages/components/Card.jsx
+++ src/pages/components/Card.jsx
@@ -4,7 +4,7 @@ export const Card = ({ title, discription, link, btnText, path }) => {
     return (
         <div>
             <article class="group flex rounded-md max-w-sm flex-col overflow-hidden border border-neutral-300 bg-neutral-50 text-neutral-600 dark:border-neutral-700 dark:bg-neutral-900 dark:text-neutral-300">
-                <div className="h-44 md:h-64 overflow-hidden bg-no-repeat bg-red-500">
+                <div className="h-44 md:h-64 overflow-hidden bg-no-repeat">
                     <img
                         src={path}
                         alt="Descriptive Alt Text"
